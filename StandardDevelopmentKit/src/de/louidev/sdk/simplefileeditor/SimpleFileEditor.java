56448b3e2a095017a25d7980d2014e2e250642f0
2705l <2705l@LAPTOP-V31G0J0F.fritz.box>
2023-07-23 12:38:58 +0200

    first commit

---
+++ StandardDevelopmentKit/src/de/louidev/sdk/simplefileeditor/SimpleFileEditor.java
@@ -0,0 +1,81 @@
+package de.louidev.sdk.simplefileeditor;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class SimpleFileEditor {
+
+	public static void write(String filePath, String content) {
+		try {
+
+			BufferedWriter bWriter = new BufferedWriter(new FileWriter(filePath));
+			bWriter.write(content);
+			bWriter.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static <O extends Serializable> void writeObject(String filePath, O object) {
+		try {
+
+			FileOutputStream fos = new FileOutputStream(filePath);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(object);
+			oos.close();
+			fos.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static Object readObject(String filePath) {
+		Object o = null;
+
+		try {
+
+			FileInputStream fis = new FileInputStream(filePath);
+			ObjectInputStream ois = new ObjectInputStream(fis);
+			o = ois.readObject();
+			ois.close();
+			fis.close();
+
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		return o;
+	}
+
+	public static String readAll(String filePath) {
+		StringBuilder sb = new StringBuilder();
+
+		try {
+
+			BufferedReader bReader = new BufferedReader(new FileReader(filePath));
+
+			String line;
+			while ((line = bReader.readLine()) != null) {
+				sb.append(line);
+			}
+
+			bReader.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		return sb.toString();
+	}
+
+}
---
+++ StandardDevelopmentKit/src/de/louidev/sdk/simpletimer/SimpleTimer.java
@@ -0,0 +1,129 @@
+package de.louidev.sdk.simpletimer;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SimpleTimer {
+
+	private Timer timer;
+	private SimpleTimerAction onCancel;
+
+	private SimpleTimer(Timer timer) {
+		this.timer = timer;
+		this.onCancel = () -> {};
+	}
+	
+	private SimpleTimer(Timer timer, SimpleTimerAction onCancel) {
+		this.timer = timer;
+		this.onCancel = onCancel;
+	}
+
+	public void cancel() {
+		timer.cancel();
+		timer.purge();
+		onCancel.performAction();
+	}
+	
+	// DELAY
+	
+	public static SimpleTimer delay(long delay, SimpleTimerAction action) {
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+
+			@Override
+			public void run() {
+				action.performAction();
+			}
+
+		}, delay);
+		return new SimpleTimer(timer);
+	}
+	
+	public static SimpleTimer delay(long delay, SimpleTimerAction action, SimpleTimerAction onCancel) {
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+
+			@Override
+			public void run() {
+				action.performAction();
+			}
+
+		}, delay);
+		return new SimpleTimer(timer, onCancel);
+	}
+	
+	// REPEAT
+	
+	public static SimpleTimer repeat(long period, long delay, SimpleTimerAction action) {
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				action.performAction();
+			}
+			
+		}, delay, period);
+		return new SimpleTimer(timer);
+	}
+	
+	public static SimpleTimer repeat(long period, long delay, SimpleTimerAction action, SimpleTimerAction onCancel) {
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				action.performAction();
+			}
+			
+		}, delay, period);
+		return new SimpleTimer(timer, onCancel);
+	}
+	
+	// REPEAT FOR
+	
+	public static SimpleTimer repeatFor(long period, long delay, int amount, SimpleTimerAction action) {
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			int x = 0;
+			
+			@Override
+			public void run() {
+				if(x == amount) {
+					timer.cancel();
+					timer.purge();
+				} else {
+					action.performAction();
+				}
+				x++;
+			}
+			
+		}, delay, period);
+		return new SimpleTimer(timer);
+	}
+	
+	public static SimpleTimer repeatFor(long period, long delay, int amount, SimpleTimerAction action, SimpleTimerAction onCancel) {
+		Timer timer = new Timer();
+		timer.schedule(new TimerTask() {
+			int x = 0;
+			
+			@Override
+			public void run() {
+				if(x == amount) {
+					timer.cancel();
+					timer.purge();
+				} else {
+					action.performAction();
+				}
+				x++;
+			}
+			
+		}, delay, period);
+		return new SimpleTimer(timer, onCancel);
+	}
+
+	public interface SimpleTimerAction {
+		void performAction();
+	}
+
+}
